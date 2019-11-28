6250d1fa8c0d4213390e190a306b399ff73c47ff
lumengxin <2735146963@qq.com>
2019-11-28 22:59:07 +0800

    获取歌手列表数据

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,15 +1,40 @@
 <template>
-  <div>
+  <div class="singer">
     singer
   </div>
 </template>
 
 <script>
+import { getSingerList } from 'api/singer'
+import { ERR_OK } from 'api/config'
+
 export default {
-  name: 'Singer'
+  name: 'Singer',
+  data () {
+    return {
+      singers: []
+    }
+  },
+  methods: {
+    _getSingerList () {
+      getSingerList().then((res) => {
+        if (res.code === ERR_OK) {
+          this.singers = res.data.list
+          console.log(this.singers)
+        }
+      })
+    }
+  },
+  created () {
+    this._getSingerList()
+  }
 }
 </script>
 
 <style lang="stylus" scoped>
-
+  .singer
+    position fixed
+    top 88px
+    bottom 0
+    width 100%
 </style>
