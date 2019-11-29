c519b7ef61dc8fe1da260f75494edd73595ace6e
lumengxin <2735146963@qq.com>
2019-11-29 21:12:23 +0800

    歌单数据列表处理

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -7,7 +7,10 @@
 <script>
 import { getSingerList } from 'api/singer'
 import { ERR_OK } from 'api/config'
+import Singer from 'assets/js/singer'
 
+const HOT_NAME = '热门'
+const HOT_SINGER_LEN = 10
 export default {
   name: 'Singer',
   data () {
@@ -20,9 +23,58 @@ export default {
       getSingerList().then((res) => {
         if (res.code === ERR_OK) {
           this.singers = res.data.list
-          console.log(this.singers)
+          console.log("TCL: _getSingerList -> this.singers", this.singers)
+          console.log(this._normalizeSinger(this.singers))
         }
       })
+    },
+    // 将原始数组加工为‘字母表’数据结构
+    _normalizeSinger (list) {
+      let map = {
+        hot: {
+          title: HOT_NAME,
+          items: []
+        }
+      }
+      list.forEach((item, index) => {
+        if (index < HOT_SINGER_LEN) {
+          map.hot.items.push(new Singer({
+            id: item.Fsinger_mid,
+            name: item.Fsinger_name
+            // avatar: `https://y.gtimg.cn/music/photo_new/T001R300x300M000${item.Fsinger_mid}.jpg?max_age=2592000`
+          }))
+        }
+        const key = item.Findex
+        if (!map[key]) {
+          map[key] = {
+            title: key,
+            items: []
+          }
+        }
+        map[key].items.push(new Singer({
+          id: item.Fsinger_mid,
+          name: item.Fsinger_name
+          // avatar: `https://y.gtimg.cn/music/photo_new/T001R300x300M000${item.Fsinger_mid}.jpg?max_age=2592000`
+        }))
+      })
+      console.log("TCL: _normalizeSinger -> map", map)
+      // 进一步处理数据，得到有序列表
+      let hot = []
+      let ret = []
+      for (let key in map) {
+        let val = map[key]
+        if (val.title.match(/[a-zA-Z]/)) {
+          ret.push(val)
+        } else if (val.title === HOT_NAME) {
+          hot.push(val)
+        }
+      }
+      ret.sort((a, b) => {
+        return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+      })
+      return hot.concat(ret)
+      console.log("TCL: _normalizeSinger -> hot", hot)
+      console.log("TCL: _normalizeSinger -> ret", ret)
     }
   },
   created () {
