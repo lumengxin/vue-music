f40476097d9fe6b8c355c6c8f49e42dc535a228e
lumengxin <2735146963@qq.com>
2019-11-30 22:59:26 +0800

    根据歌手id获取到歌手详情页数据

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -8,16 +8,34 @@
 
 <script>
 import { mapGetters } from 'vuex'
+import { getSingerDetail } from 'api/singer'
+import { ERR_OK } from 'api/config'
 
 export default {
   name: 'SingerDetail',
   computed: {
+    // 从vuex中store中获取singer数据
     ...mapGetters([
       'singer'
     ])
   },
+  methods: {
+    _getDetail () {
+      // 直接路由进入或者刷新，得不到歌手详情数据。回退出去，点击进入
+      if (!this.singer.id) {
+        this.$router.push('/singer')
+        return
+      }
+      getSingerDetail(this.singer.id).then((res) => {
+        if (res.code === ERR_OK) {
+          console.log(res.data.list)
+        }
+      })
+    }
+  },
   created () {
     console.log(this.singer)
+    this._getDetail()
   }
 }
 </script>
