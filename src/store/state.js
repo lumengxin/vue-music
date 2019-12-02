e4f8538a012defffc4ea665028f69bc62df64d84
lumengxin <2735146963@qq.com>
2019-12-02 20:52:23 +0800

    播放器vuex数据设计

--- src/store/state.js
+++ src/store/state.js
@@ -1,5 +1,20 @@
+import { playMode } from 'assets/js/config'
+
 const state = {
-  singer: {}
+  singer: {},
+  /* 播放器公共数据 */
+  // 播放状态
+  playing: false,
+  // 是否全屏
+  fullScreen: false,
+  // 播放列表
+  playlist: [],
+  // 顺序列表
+  sequenceList: [],
+  // 播放模式
+  mode: playMode.sequence,
+  // 当前播放索引
+  currentIndex: -1
 }
 
 export default state
