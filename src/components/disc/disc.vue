11873064bbd65b208b3e06d70851fc63a45fc826
lumengxin <2735146963@qq.com>
2020-01-23 22:56:41 +0800

    完成推荐页歌单详情列表

--- src/components/disc/disc.vue
+++ src/components/disc/disc.vue
@@ -1,6 +1,6 @@
 <template>
   <transition name="slide">
-    <music-list :title="title" :bg-image="bgImage"></music-list>
+    <music-list :title="title" :bg-image="bgImage" :songs="songs"></music-list>
   </transition>
 </template>
 
@@ -9,6 +9,7 @@ import MusicList from 'components/music-list/music-list'
 import { mapGetters } from 'vuex'
 import { getRecommendSongList } from 'api/recommend'
 import { ERR_OK } from 'api/config'
+import { createSong } from 'assets/js/song'
 
 export default {
   components: {
@@ -25,13 +26,33 @@ export default {
       'disc'
     ])
   },
+  data () {
+    return {
+      songs: []
+    }
+  },
   methods: {
     _getRecommendSongList () {
+      if (!this.disc.dissid) {
+        this.$router.push('/recommend')
+        return
+      }
       getRecommendSongList(this.disc.dissid).then((res) => {
         if (res.code === ERR_OK) {
           console.log(res.cdlist[0].songlist)
+          this.songs = this._normalizeSongs(res.cdlist[0].songlist)
+          console.log(this.songs)
         }
       })
+    },
+    _normalizeSongs (list) {
+      let ret = []
+      list.forEach(musicData => {
+        if (musicData.songid && musicData.albumid) {
+          ret.push(createSong(musicData))
+        }
+      })
+      return ret
     }
   },
   created () {
