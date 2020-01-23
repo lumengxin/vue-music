a7952a9c5eaa0507cd5344d1400c7f30fd3d2fdb
lumengxin <2735146963@qq.com>
2020-01-23 22:44:23 +0800

    获取到推荐页中歌单详情数据

--- src/api/recommend.js
+++ src/api/recommend.js
@@ -42,3 +42,41 @@ export function getDiscList () {
     console.log(err)
   })
 }
+
+// 推荐页歌单列表数据获取
+// export function getRecommendSongList (disstid) {
+//   const url = 'https://c.y.qq.com/qzone/fcg-bin/fcg_ucc_getcdinfo_byids_cp.fcg'
+
+//   const data = Object.assign({}, commonParams, {
+//     disstid,
+//     type: 1,
+//     json: 1,
+//     utf8: 1,
+//     onlysong: 0,
+//     platform: 'yqq',
+//     hostUin: 0,
+//     needNewCode: 0
+//   })
+
+//   return jsonp(url, data, options)
+// }
+export function getRecommendSongList (disstid) {
+  const url = '/api/getCdInfo'
+
+  const data = Object.assign({}, commonParams, {
+    disstid,
+    type: 1,
+    json: 1,
+    utf8: 1,
+    onlysong: 0,
+    platform: 'yqq',
+    hostUin: 0,
+    needNewCode: 0
+  })
+
+  return axios.get(url, {
+    params: data
+  }).then((res) => {
+    return Promise.resolve(res.data)
+  })
+}
--- src/components/disc/disc.vue
+++ src/components/disc/disc.vue
@@ -7,6 +7,8 @@
 <script>
 import MusicList from 'components/music-list/music-list'
 import { mapGetters } from 'vuex'
+import { getRecommendSongList } from 'api/recommend'
+import { ERR_OK } from 'api/config'
 
 export default {
   components: {
@@ -22,6 +24,18 @@ export default {
     ...mapGetters([
       'disc'
     ])
+  },
+  methods: {
+    _getRecommendSongList () {
+      getRecommendSongList(this.disc.dissid).then((res) => {
+        if (res.code === ERR_OK) {
+          console.log(res.cdlist[0].songlist)
+        }
+      })
+    }
+  },
+  created () {
+    this._getRecommendSongList()
   }
 }
 </script>
