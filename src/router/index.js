c35325c623f1835dabe92caf4101658920234bee
lumengxin <2735146963@qq.com>
2020-01-28 11:04:04 +0800

    路由懒加载

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Rank from 'components/rank/rank'
-import Recommend from 'components/recommend/recommend'
-import Search from 'components/search/search'
-import Singer from 'components/singer/singer'
-import SingerDetail from 'components/singer-detail/singer-detail'
-import Disc from 'components/disc/disc'
-import TopList from 'components/top-list/top-list'
-import UserCenter from 'components/user-center/user-center'
+// import Rank from 'components/rank/rank'
+// import Recommend from 'components/recommend/recommend'
+// import Search from 'components/search/search'
+// import Singer from 'components/singer/singer'
+// import SingerDetail from 'components/singer-detail/singer-detail'
+// import Disc from 'components/disc/disc'
+// import TopList from 'components/top-list/top-list'
+// import UserCenter from 'components/user-center/user-center'
+
+// 路由懒加载（老版本）, 将app.js分割为更多js, 路由组件按需异步加载
+// const Recommend = (resolve) => {
+//   import('components/recommend/recommend').then(recommend => {
+//     resolve(recommend)
+//   })
+// }
 
 Vue.use(Router)
 
@@ -20,48 +27,48 @@ export default new Router({
     },
     {
       path: '/rank',
-      component: Rank,
+      component: () => import('components/rank/rank'),
       children: [
         {
           path: ':id',
-          component: TopList
+          component: () => import('components/top-list/top-list')
         }
       ]
     },
     {
       path: '/recommend',
-      component: Recommend,
+      component: () => import('components/recommend/recommend'),
       children: [
         {
           path: ':id',
-          component: Disc
+          component: () => import('components/disc/disc')
         }
       ]
     },
     {
       path: '/search',
-      component: Search,
+      component: () => import('components/search/search'),
       children: [
         {
           path: ':id',
-          component: SingerDetail
+          component: () => import('components/singer-detail/singer-detail')
         }
       ]
     },
     {
       path: '/singer',
       name: 'singer',
-      component: Singer,
+      component: () => import('components/singer/singer'),
       children: [
         {
           path: ':id',
-          component: SingerDetail
+          component: () => import('components/singer-detail/singer-detail')
         }
       ]
     },
     {
       path: '/user',
-      component: UserCenter
+      component: () => import('components/user-center/user-center')
     }
   ]
 })
